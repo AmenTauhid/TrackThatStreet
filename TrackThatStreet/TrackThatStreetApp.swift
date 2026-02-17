029697d677a260f5338e68368272cefb11fad777
Ayman Tauhid <aymantauhid2@gmail.com>
2026-02-16 22:18:54 -0500

    Initial Commit

---
+++ TrackThatStreet/TrackThatStreetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackThatStreetApp.swift
+//  TrackThatStreet
+//
+//  Created by Ayman Tauhid on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct TrackThatStreetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
