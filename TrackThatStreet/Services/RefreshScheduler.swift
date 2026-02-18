3ff69eac82bfebb660a93d8c6a10e9350ba1a03d
Ayman Tauhid <aymantauhid2@gmail.com>
2026-02-17 23:36:16 -0500

    Add refresh scheduler for polling updates

---
+++ TrackThatStreet/Services/RefreshScheduler.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+@Observable
+final class RefreshScheduler {
+    private var timer: Timer?
+    private(set) var isRunning = false
+
+    func start(interval: TimeInterval = 25, action: @escaping @Sendable () async -> Void) {
+        stop()
+        isRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+            Task { await action() }
+        }
+    }
+
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+        isRunning = false
+    }
+}
